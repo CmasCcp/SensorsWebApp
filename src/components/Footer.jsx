c5889d849c7268087fc5e4f03ef8bdd6ffea2492
simon <=sariasneira@gmail.com>
2024-09-27 13:10:47 -0300

    Changed UI and Looker

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,60 +1,9 @@
 
 export const Footer = () => {
     return (
-        <div style={{"minHeight":"270px"}} className="overflow-hidden px-5 m-0 w-100 row justify-content-around align-items-center bg-customprimary text-light">
-
-            <div className="d-flex mt-3 overflow-hidden flex-column justify-content-start align-items-center m-0" style={{"maxWidth": "200px"}}>
-                <img height="20" className='my-3' src='https://centrodeinvestigacioningenieria.udd.cl/wp-content/uploads/2021/09/logo-udd-blanco.png'/>
-                <img style={{"maxHeight": "50px", "maxWidth": "200px"}} className="mx-auto" src="https://centrodeinvestigacioningenieria.udd.cl/wp-content/uploads/2021/09/logo-acreditacion-blanco-300x86.png"/>
-
-            </div>
-
-
-            <div className="container mt-auto p-5 col-md-4 d-flex flex-column justify-content-start align-items-center text-center">
-                {/* <?php if(!empty($info->email)||!empty($info->fono)):?> */}
-
-                {/* <h5 className="text-center">Datos de contacto</h5> */}
-                
-
-                <h3>Sede Concepción</h3>
-                <p>Ainavillo 456, Concepción</p>
-                <p>(+56-41) 226 8610</p>
-
-                    {/* <?php if(!empty($info->fono)):?> */}
-                    {/* <?php echo $info->fono."<br>";?> */}
-                        {/* <?php endif;?> */}
-                        {/* <?php if(!empty($info->email)):?>
-                        <?php echo $info->email."<br>";?>
-                            <?php endif;?> */}
-                
-
-                        
-                        
-                        {/* <h5 className="text-center">Redes Sociales</h5> */}
-                        <div className="d-flex row mt-2">
-                            {/* <!-- facebook --> */}
-                            {/* <a target="_blank" className="bg-white text-customprimary mx-1 icon-fab-light rounded-circle text-center" href="<?php echo $info->facebook;?>">
-                                <span className=""><i className="fab fa-facebook"></i></span>
-                            </a> */}
-
-
-                            {/* <a target="_blank" className="bg-danger mx-1 icon-fab-light rounded-circle text-center" href="<?php echo $info->youtube;?>&text=Hola, quiero hacer mi pedido.">
-                                <span className=""><i className="fab fa-whatsapp"></i></span>
-                            </a> */}
-
-                            {/* <a target="_blank" className="bg-white text-customprimary mx-1 icon-fab-light rounded-circle text-center" href="<?php echo $info->instagram;?>">
-                                <span className=""><i className="fab fa-instagram"></i></span>
-                            </a> */}
-                        </div>
-                        {/* <?php endif;?> */}
-
-            </div>
-            <div className="col-md-4 d-flex flex-column">
-            <p className="mx-auto pt-5  text-center justify-self-end"><small>&copy; 
-                { new Date().getFullYear() }
-                &nbsp;
-                <a href="#" className="text-white">Desarrollado por DK</a></small></p>
-            </div>
-        </div>
+        <footer className="footer">
+            <p>© 2024 Centro de investigación en tecnologías para la sociedad (C+).</p>
+            <p>Todos los derechos reservados.</p>
+      </footer>
     )
 }
--- src/pages/SensoresPage.jsx
+++ src/pages/SensoresPage.jsx
@@ -1,58 +1,14 @@
-import { AreaChartGraphic } from '../components/graphics/AreaChartGraphic';
-import { BarGraphic } from '../components/graphics/BarGraphic';
-import { CardGraphic } from '../components/graphics/CardGraphic';
-import { DataTableGraphic } from "../components/graphics/DataTableGraphic"
-
-
-
+import IframeResizer from '@iframe-resizer/react';
 
 export const SensoresPage = ({ widthClose }) => {
-    return (
-        <div className="container-fluid m-0" id="layoutSidenav_content" style={widthClose ? {} : { "marginLeft": "-225px" }}>
-            <main>
-                <div className="container-fluid px-4">
-                    <h1 className="mt-4">Dashboard</h1>
-
-                    <div className="row mt-4">
-                        <div className="col-md-12">
-                            <iframe
-                                title="Dashboard"
-                                width="100%"
-                                height="600"
-                                src="https://lookerstudio.google.com/embed/reporting/6b1d3791-542f-4c15-8c6e-5fcb0a7664f6/page/WuE6D"
-                                allowFullScreen
-                                sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
-                            ></iframe>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-xl-6">
-                            <div className="card mb-4">
-                                <div className="card-header">
-                                    <i className="fas fa-chart-area me-1"></i>
-                                    Area Chart Example
-                                </div>
-                                <div className="card-body">
-                                    {/* <AreaChartGraphic apiUrl={"https://mindicador.cl/api/dolar"} /> */}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-6">
-                            <div className="card mb-4">
-                                <div className="card-header">
-                                    <i className="fas fa-chart-bar me-1"></i>
-                                    Bar Chart Example
-                                </div>
-                                <div className="card-body">
-                                    {/* <BarGraphic /> */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </main>
+  return (
+    <div className="container-fluid d-flex justify-content-center align-items-center">
+      <div className="card">
+        <h2 className="card-title">Dashboard</h2>
+        <div className="card-content">
+        <iframe title="EDA" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=42af7c04-324d-422f-bc69-6d3718b9e09b&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
